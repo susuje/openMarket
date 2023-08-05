5b57c3fe7d9f1f37950a0e364356a630ed6411c5
susuje <wjdtn0n@naver.com>
2023-08-05 23:17:09 +0900

    :lipstick: 코드 수정

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef } from 'react'
 import * as S from './Carousel.style'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
@@ -14,9 +14,9 @@ export default function Carousel() {
     { src: banner3, alt: '세번째 배너 사이트소개' },
   ])
   const settings = {
-    dots: true, // 캐러셀 밑의 지정 콘텐츠로 이동할 수 있는 버튼
+    dots: true, // 캐러셀 밑 버튼
     infinite: true, // 콘텐츠 맨 끝에서 다시 앞으로 반복
-    speed: 500, // 콘텐츠 넘어가는 속도 (ms)
+    speed: 1200, // 콘텐츠 넘어가는 속도 (ms)
     slidesToShow: 1, // 한화면에 보이는 콘텐츠 개수
     slidesToScroll: 1, // 한번에 넘어가는 콘텐츠 수
     autoplay: true,
