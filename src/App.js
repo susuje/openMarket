49da060e73d89abe72607f9ed242d7ef031e131a
susuje <wjdtn0n@naver.com>
2023-08-05 23:16:28 +0900

    :sparkles: 라우트 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
