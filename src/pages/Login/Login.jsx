0c424389ecd88868b44f20d8f4965fc09b8c308a
susuje <wjdtn0n@naver.com>
2023-08-07 22:15:03 +0900

    :sparkles: 로그인 페이지 구현

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import * as S from './Login.style'
+import * as S from './LoginSign.style'
 
 import logo from '../../assets/icon/bigLogo.svg'
-import LoginForm from '../../components/LoginForm/LoginForm'
+import LoginForm from '../../components/LoginSignForm/LoginForm'
 import TabBtn from '../../components/TabBtn/TabBtn'
 
 export default function Login() {
   return (
     <S.Container>
       <S.Img src={logo} alt="로고" />
-      <TabBtn />
+      <TabBtn content={'로그인'} />
       <LoginForm />
+      <S.Footer>
+        <a href="/signup">회원가입</a>
+        <a href="/">비밀번호 찾기</a>
+      </S.Footer>
     </S.Container>
   )
 }
---
+++ src/pages/Login/LoginSign.style.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Img = styled.img`
+  width: 334px;
+  height: 95px;
+  margin-bottom: 50px;
+`
+export const Container = styled.div`
+  width: max-content;
+  text-align: center;
+
+  //가운데배치
+  margin: 0 auto;
+  padding: 50px;
+`
+export const Footer = styled.footer`
+  margin-top: 30px;
+  font-size: 16px;
+  > a:first-child {
+    &::after {
+      content: '|';
+      margin: 0px 14px;
+    }
+  }
+`
