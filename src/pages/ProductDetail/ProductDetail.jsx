cfd5de326b6d8d06b9aa95465d965c3e4545e5fc
susuje <wjdtn0n@naver.com>
2023-08-10 22:01:00 +0900

    :tada: ProductDetail 페이지 추가

---
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './ProductDetail.style'
+import TopNavBar from '../../components/TopNav/TopNavBar'
+export default function ProductDetail() {
+  return (
+    <>
+      <TopNavBar />
+    </>
+  )
+}
