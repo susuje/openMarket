3b4a922083dfe97bcecb93bb66e2854cac2b5478
susuje <wjdtn0n@naver.com>
2023-08-11 22:24:00 +0900

    :sparkles: 프로덕트 상세 페이지 마크업 구현

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -2,10 +2,41 @@ import React from 'react'
 
 import * as S from './ProductDetail.style'
 import TopNavBar from '../../components/TopNav/TopNavBar'
+import sampleImg from '../../assets/img/voyage.jpg'
+import AmountBtn from '../../components/Product/AmountBtn'
+
 export default function ProductDetail() {
   return (
     <>
       <TopNavBar />
+      <S.Container>
+        <S.Img src={sampleImg} />
+        <S.InfoWrapper>
+          <p>Claire_LeMarket</p>
+          <S.ProductName>14인치 기내용 어린이 캐리어 - 블루</S.ProductName>
+          <p className="won">
+            <S.Price>85,000</S.Price>원
+          </p>
+          <p className="ship">택배배송 / 무료배송</p>
+          <S.Div>
+            <AmountBtn />
+          </S.Div>
+          <S.TotalPriceDiv>
+            <p>총 상품 금액</p>
+            <div>
+              <p>
+                총 수량 <span>1</span>개
+              </p>
+              <p>|</p>
+              <strong>
+                <S.Price>17,500</S.Price>원
+              </strong>
+            </div>
+          </S.TotalPriceDiv>
+          <S.Btn>바로 구매</S.Btn>
+          <S.Btn className="cart">장바구니</S.Btn>
+        </S.InfoWrapper>
+      </S.Container>
     </>
   )
 }
--- src/pages/ProductDetail/ProductDetail.style.jsx
+++ src/pages/ProductDetail/ProductDetail.style.jsx
@@ -1,1 +1,99 @@
 import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  width: 1280px;
+  margin: 80px auto 0px auto;
+`
+
+export const Img = styled.img`
+  width: 600px;
+  height: 600px;
+  border-radius: 20px;
+`
+export const InfoWrapper = styled.div`
+  margin-left: 50px;
+  width: 630px;
+  p {
+    color: var(--light-grey);
+    font-size: 18px;
+  }
+  p.won {
+    color: #000;
+    margin-bottom: 161px;
+  }
+  p.ship {
+    font-size: 16px;
+    padding-bottom: 20px;
+  }
+`
+
+export const ProductName = styled.h4`
+  font-size: 36px;
+  margin-top: 16px;
+  margin-bottom: 20px;
+`
+
+export const TotalPriceDiv = styled.div`
+  margin-top: 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 22px;
+  p {
+    font-size: 18px;
+    font-weight: 700;
+    color: #000;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    p {
+      font-size: 18px;
+      font-weight: 400;
+      margin-right: 12px;
+      color: var(--light-grey);
+      + p {
+        color: var(--grey-border);
+      }
+      span {
+        font-weight: 700;
+        color: #000;
+      }
+    }
+
+    strong {
+      font-size: 18px;
+    }
+  }
+`
+
+export const Price = styled.span`
+  font-size: 36px;
+  font-weight: 700;
+  color: #000;
+  margin-right: 2px;
+`
+
+export const Div = styled.div`
+  padding: 30px 0px 30px 0px;
+  border-top: 2px solid var(--grey-border);
+  border-bottom: 2px solid var(--grey-border);
+`
+
+export const Btn = styled.button`
+  background-color: #000;
+  color: white;
+  width: 416px;
+  height: 60px;
+  border-radius: 5px;
+  font-size: 18px;
+  font-weight: 700;
+  margin-top: 10px;
+  &.cart {
+    background-color: var(--light-grey);
+    width: 200px;
+    margin-left: 14px;
+  }
+`
