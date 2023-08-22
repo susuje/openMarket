deae2eac2f3e2d9eb30ccd77ff1aac057aca0344
susuje <wjdtn0n@naver.com>
2023-08-22 16:50:18 +0900

    :sparkles: 최종결제정보 컴포넌트 구현

--- src/components/payment/TotalPayment.jsx
+++ src/components/payment/TotalPayment.jsx
@@ -1,5 +1,48 @@
 import React from 'react'
+import * as S from './TotalPayment.style'
+
+import unchecked from '../../assets/icon/unchecked-box.svg'
+import checked from '../../assets/icon/check-fill-box.svg'
 
 export default function TotalPayment() {
-  return <div>TotalPayment</div>
+  return (
+    <>
+      <S.H3>최종결제 정보</S.H3>
+      <S.Container>
+        <S.FirstDiv>
+          <S.FlexDiv>
+            <p>- 상품금액</p>
+            <p>
+              <span>46,500</span>원
+            </p>
+          </S.FlexDiv>
+          <S.FlexDiv>
+            <p>- 할인금액</p>
+            <p>
+              <span>0</span>원
+            </p>
+          </S.FlexDiv>
+          <S.FlexDiv className="ship">
+            <p>- 배송비</p>
+            <p>
+              <span>2,500</span>원
+            </p>
+          </S.FlexDiv>
+          <S.FlexDiv>
+            <p>- 결제금액</p>
+            <strong>49,000원</strong>
+          </S.FlexDiv>
+        </S.FirstDiv>
+        <S.LastDiv>
+          <div>
+            <input type="checkbox" id="check" required />
+            <label htmlFor="check">
+              주문 내용을 확인하였으며, 정보 제공 등에 동의합니다.
+            </label>
+          </div>
+          <S.Button>결제하기</S.Button>
+        </S.LastDiv>
+      </S.Container>
+    </>
+  )
 }
---
+++ src/components/payment/TotalPayment.style.jsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components'
+import checked from '../../assets/icon/check-fill-box.svg'
+export const H3 = styled.h3`
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 18px;
+`
+export const Container = styled.div`
+  border: 2px solid #000000;
+  border-radius: 10px;
+  width: 480px;
+  overflow: hidden;
+`
+export const FirstDiv = styled.div`
+  padding: 30px 30px 20px 30px;
+`
+
+export const FlexDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+
+  &.ship {
+    margin-bottom: 0px;
+    padding-bottom: 19px;
+    border-bottom: 1px solid var(--grey-border);
+  }
+  &:last-child {
+    margin-top: 24px;
+    margin-bottom: 0px;
+  }
+
+  p {
+    font-size: 16px;
+    span {
+      font-size: 18px;
+      font-weight: 700;
+      color: #000000;
+      margin-right: 4px;
+    }
+  }
+  p:last-child {
+    font-size: 14px;
+    color: var(--light-grey);
+  }
+  strong {
+    font-size: 24px;
+    font-weight: 700;
+    color: #eb5757;
+  }
+`
+
+export const LastDiv = styled.div`
+  background-color: #f2f2f2;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  input {
+    display: none;
+  }
+  label {
+    width: 16px;
+    height: 16px;
+    background: url(${checked}) no-repeat left/16px 16px;
+    padding-left: 30px;
+  }
+`
+
+export const Button = styled.button`
+  color: white;
+  background: #000000;
+  font-size: 24px;
+  font-weight: 700;
+  padding: 19px 65px;
+  border-radius: 5px;
+  margin-top: 30px;
+  margin-bottom: 4px;
+`
