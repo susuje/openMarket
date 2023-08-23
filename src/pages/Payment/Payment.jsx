f3c379ff40da0f2929663feb988d2b4a988736c7
susuje <wjdtn0n@naver.com>
2023-08-23 11:35:08 +0900

    :sparkles: 결제 페이지 마크업 구현

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -3,12 +3,26 @@ import * as S from './Payment.style'
 
 import TotalPayment from '../../components/payment/TotalPayment'
 import ProductList from '../../components/payment/ProductList'
+import ShippingForm from '../../components/payment/ShippingForm'
 
 export default function Payment() {
   return (
-    <>
+    <S.Wrapper>
+      <S.CartInfoBar>
+        <p>상품정보</p>
+        <div>
+          <p>할인</p>
+          <p>배송비</p>
+          <p>주문금액</p>
+        </div>
+      </S.CartInfoBar>
       <ProductList />
-      <TotalPayment />
-    </>
+      <ProductList />
+      <S.TotalPrice>
+        총 주문금액
+        <strong>260,000원</strong>
+      </S.TotalPrice>
+      <ShippingForm />
+    </S.Wrapper>
   )
 }
--- src/pages/Payment/Payment.style.jsx
+++ src/pages/Payment/Payment.style.jsx
@@ -1,1 +1,39 @@
 import styled from 'styled-components'
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+`
+export const CartInfoBar = styled.div`
+  background-color: #f2f2f2;
+  display: grid;
+  width: 100%;
+  max-width: 1280px;
+  border-radius: 10px;
+  grid-template-columns: 1fr 1fr;
+  text-align: center;
+  align-self: center;
+  padding: 18px 0px;
+  p {
+    font-size: 18px;
+  }
+  div {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`
+export const TotalPrice = styled.p`
+  font-size: 18px;
+  width: 100%;
+  max-width: 1280px;
+  font-weight: 500;
+  text-align: right;
+  margin-top: 30px;
+
+  strong {
+    font-size: 24px;
+    color: var(--red);
+    font-weight: 700;
+    margin-left: 10px;
+  }
+`
