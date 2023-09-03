d63198cdb7613d219708600310a32dc842e773bc
susuje <wjdtn0n@naver.com>
2023-09-03 23:35:21 +0900

    :sparkles: react-query Provider 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 // import ReactDOM from 'react-dom'; //구버전
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import GlobalStyle from './styles/GlobalStyle'
@@ -7,10 +9,11 @@ import GlobalFont from './styles/GlobalFont'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
+const queryClient = new QueryClient()
 root.render(
-  <>
+  <QueryClientProvider client={queryClient}>
     <GlobalFont />
     <GlobalStyle />
     <App />
-  </>
+  </QueryClientProvider>
 )
