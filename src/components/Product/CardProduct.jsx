80863397a24bed6129a60b587de1e6e20c3eb5d9
susuje <wjdtn0n@naver.com>
2023-10-08 15:25:59 +0900

    :lipstick: 반응형 카드 마크업

--- src/components/Product/CardProduct.jsx
+++ src/components/Product/CardProduct.jsx
@@ -8,7 +8,7 @@ export default function CardProduct() {
   return (
     <S.Card>
       <S.SellerName>Claire_LeMarket</S.SellerName>
-      <S.Img src={glass} />
+      <S.ImgContainer></S.ImgContainer>
       <S.ProductName>화이트 유리화병 오브제</S.ProductName>
       <S.ProductPrice>29,000원</S.ProductPrice>
       <S.CartBtn>
--- src/components/Product/CardProduct.style.js
+++ src/components/Product/CardProduct.style.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components'
+import glass from '../../assets/img/glass.jpg'
 
-export const Card = styled.div`
-  width: 350px;
-  height: 460px;
-  border-radius: 16px;
-  padding: 14px 8px;
-  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
-    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+export const Card = styled.li`
+  /* width: 350px;
+  height: 460px; */
+
+  border-radius: 1rem;
+  padding: 1rem 1rem 1.5rem 1rem;
+  box-shadow: rgba(60, 64, 67, 0.1) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.1) 0px 2px 6px 2px;
   position: relative;
 `
 export const SellerName = styled.p`
@@ -15,6 +17,12 @@ export const SellerName = styled.p`
   margin-left: 10px;
   color: var(--light-grey);
   margin-bottom: 12px;
+`
+export const ImgContainer = styled.div`
+  padding-top: 95%;
+  background: url(${glass}) no-repeat center;
+  background-size: cover;
+  border-radius: 1rem;
 `
 export const ProductName = styled.p`
   color: var(--light-grey);
@@ -29,14 +37,9 @@ export const ProductPrice = styled.p`
   font-weight: 700;
   margin-left: 10px;
 `
-export const Img = styled.img`
-  width: 334px;
-  height: 295px;
-  object-fit: cover;
-  border-radius: 16px;
-`
+
 export const CartBtn = styled.button`
   position: absolute;
-  right: 20px;
+  right: 30px;
   top: 55px;
 `
