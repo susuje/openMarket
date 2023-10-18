68c7549a1c784c2417542406c30c159df4d6478f
susuje <wjdtn0n@naver.com>
2023-10-18 19:42:58 +0900

    :sparkles: 모달창 폴더 생성

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import * as S from './Modal.style'
+export default function Modal() {
+  return <S.Container>Modal</S.Container>
+}
---
+++ src/components/Modal/Modal.style.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 5;
+  background-color: rgba(0, 0, 0, 0.3);
+`
