f4dcf0fd3e84aa61aeb23abb74ff97946ba62c9d
susuje <wjdtn0n@naver.com>
2023-11-15 20:35:20 +0900

    :lipstick: css 수정

--- src/pages/Payment/Payment.style.jsx
+++ src/pages/Payment/Payment.style.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   width: 100%;
   max-width: 1280px;
-  margin: 0 auto;
+  margin: 100px auto 0px auto;
 `
 export const CartInfoBar = styled.div`
   background-color: #f2f2f2;
